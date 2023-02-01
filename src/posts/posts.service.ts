0530543f801438ed9719c32ad1978bfc7fac8801
Карабаев Нуриддин <94254058+asuramesocrum@users.noreply.github.com>
2023-02-01 18:17:55 +0400

    upd
    correction

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -7,54 +7,65 @@ import {PaginationPostDto} from './dto/pagination.dto';
 @Injectable()
 export class PostsService {
 
-    async getAll_p(@Param() params){
-        const client = new PrismaClient();
-        await client.$connect();
-        const skip = params.limit * (params.page - 1)
-        const res = await client.post.findMany({skip: skip, take: Number(params.limit)})
-        return res;
+    constructor() {
+        // Мы очень часто дублируем код и каждый раз создаём запрос на подключение к БД, не оптимально.
+        // Почитай доку по prisma в nestjs там объясняется как использовать prisma с nestjs.
+        this.client = new PrismaClient();
+        await this.client.$connect();
+    }
+    
+    // Декоратор лишний при получении данных.
+    // Не получай не типизированные данные в функции.
+    async getAll_p(page: number, limit: number){
+        const skip = limit * (page - 1)
+        // Сокрашай код ввезде где можно, если при этом не нарушится читаемость.
+        // Из за того что ты не типизировал данные на входе тебе приходится типизировать в процессе, это не правильно функция приняла не чистые данные.
+        return await this.client.post.findMany({skip: skip, take: limit})
     }
 
     async getAll(){
-        const client = new PrismaClient();
-        await client.$connect();
-        const res = await client.post.findMany()
-        return res;
+        return await this.client.post.findMany()
     }
 
-    async getById(@Param() params){
-        const client = new PrismaClient();
-        await client.$connect();
-        const res = await client.post.findMany({ where: {id: Number(params.id)}})
-        return res;
+    // Декоратор лишний при получении данных.
+    async getById(id: number){
+        return await this.client.post.findMany({ where: {id: id}})
     }
 
     async createTenThounds(){
-        const client = new PrismaClient();
-        const array = []
-        for(let i = 1; i <= 10000; i++){
-            const container = {
-                title: `Title num: ${i}`,
-                description: `description num: ${i}`,
-            };
-            array.push(container);
+        try {
+            const array = []
+            for(let i = 1; i <= 10000; i++){
+                const container = {
+                    title: `Title num: ${i}`,
+                    description: `description num: ${i}`,
+                };
+                array.push(container);
+            }
+            await this.client.post.createMany({data: array})
+            return 'Done';
+        } catch (e) {
+            console.log(e)
         }
-        await client.$connect();
-        await client.post.createMany({data: array})
-        return 'Done';
     }
 
-    async deleteById(@Param() params){
-        const client = new PrismaClient();
-        await client.$connect();
-        const post = await client.post.delete({where: {id: Number(params.id)}})
-        return `Deleted post #${post.id}`
+    async deleteById(id: number){
+        // Все методы которые могут вызвать ошибку, при котором приложение умрёт оборачивай в try catch и даже если бд умрёт приложение будет жить
+        try {
+            const post = await this.client.post.delete({where: {id: id}})
+            return `Deleted post #${post.id}`
+        } catch (e) {
+            console.log(e)
+        }
     }
 
-    async updateById(@Param() params, @Body() UpdatePostDto: UpdatePostDto){
-        const client = new PrismaClient();
-        await client.$connect();
-        const post = await client.post.update({where: {id: Number(params.id)}, data: { title: UpdatePostDto.title, description: UpdatePostDto.description}})
-        return post
+    
+    async updateById(id: number, data: UpdatePostDto){
+        // Все методы которые могут вызвать ошибку, при котором приложение умрёт оборачивай в try catch и даже если бд умрёт приложение будет жить
+        try {
+            return await this.client.post.update({where: {id: id}, data: { title: data.title, description: data.description}})
+        } catch (e) {
+            console.log(e)
+        }
     }
 }
