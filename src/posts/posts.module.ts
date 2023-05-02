d64ee4c2bdeb7cd819146b12fb895dc7bfa0169e
alesin9x <131656710+alesin9x@users.noreply.github.com>
2023-05-02 13:24:34 +0400

    major update
    jwt auth
    users
    guards

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import {PrismaService} from '../prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService, PrismaService]
+  providers: [PostsService, PrismaService, JwtService]
 })
 export class PostsModule {}
