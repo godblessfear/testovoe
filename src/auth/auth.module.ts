b346838c3486649aa8e439bea894464a904027a8
Alexander <fear1reportbot@gmail.com>
2023-05-02 20:06:22 +0400

    JWT Strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,17 @@ import { UsersService } from 'src/users/users.service';
 import { PrismaService } from 'src/prisma.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getJWTConfig } from './jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UsersService, PrismaService],
+  providers: [AuthService, UsersService, PrismaService, JwtStrategy],
   imports: [JwtModule.registerAsync({
     imports: [ConfigModule],
     inject: [ConfigService],
     useFactory: getJWTConfig
-  })]
+  }),
+  PassportModule]
 })
 export class AuthModule {}
---
+++ src/auth/strategies/jwt.strategy.js
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt } from "passport-jwt";
+import { Strategy } from "passport-jwt";
+import { User } from '@prisma/client';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor () {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: process.env.JWT_SECRET
+        });
+    }
+
+    async validate( payload){
+        return payload
+    }
+}
